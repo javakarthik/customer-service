05d187e2893052652572811f88fe2298f737ab69
Karthik Gunasekaran <gunakarthik@gmail.com>
2020-05-28 18:22:54 +0530

    commit

--- src/main/java/com/demo/ReactiveServerConfig.java
+++ src/main/java/com/demo/ReactiveServerConfig.java
@@ -5,16 +5,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 @Configuration
 @EnableConfigurationProperties(DemoProperties.class)
 public class ReactiveServerConfig {
 
+	/*@Autowired
+	private HeaderExchange headerExchange;
+
     @Bean
-    public WebClient webClient(DemoProperties demoProperties, HeaderExchange headerExchange) {
+    public WebClient webClient() {
         return WebClient
                 .builder()
                 .filter(headerExchange)
                 .baseUrl(System.getenv("order-service"))
                 .build();
-    }
+    }*/
 }
--- src/main/java/com/demo/customer/CustomerController.java
+++ src/main/java/com/demo/customer/CustomerController.java
@@ -4,6 +4,7 @@ import brave.sampler.Sampler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,17 +14,27 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import org.springframework.http.MediaType;
 
+
+
 import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
+import com.demo.DemoProperties;
+import com.demo.HeaderExchange;
+
 
 @EnableAutoConfiguration
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class CustomerController {
 
-    @Autowired
-    WebClient webClient;
+  
+	DemoProperties dp = new DemoProperties();
+	HeaderExchange headerExchange = new HeaderExchange(dp);
+	
+	@Autowired
+    private WebClient webClient;
 
 
     @GetMapping(value = "/customers/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
@@ -50,6 +61,18 @@ public class CustomerController {
     WebClient webClient() {
         return WebClient.create(System.getenv("order-service"));
     }*/
+	@Bean
+     WebClient webClient() {
+		String upStreamUrl = System.getenv("order-service");
+	    if(upStreamUrl == null || upStreamUrl.isEmpty())
+		upStreamUrl = "http://localhost:8082";
+		
+        return WebClient
+                .builder()
+                //.filter(headerExchange)
+                .baseUrl(upStreamUrl)
+                .build();
+    }
 
 
 }
