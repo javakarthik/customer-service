bb539ce9da104c2e0ab1409b48c4c492e812db93
javakarthik <32713545+javakarthik@users.noreply.github.com>
2020-05-28 15:04:02 +0530

    Create ReactiveServerConfig.java

---
+++ src/main/java/com/demo/ReactiveServerConfig.java
@@ -0,0 +1,20 @@
+package com.demo;
+import com.demo.reactiveserver.config.exchange.HeaderExchange;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@EnableConfigurationProperties(DemoProperties.class)
+public class ReactiveServerConfig {
+
+    @Bean
+    public WebClient webClient(DemoProperties demoProperties, HeaderExchange headerExchange) {
+        return WebClient
+                .builder()
+                .filter(headerExchange)
+                .baseUrl(demoProperties.getBackendUri())
+                .build();
+    }
+}
